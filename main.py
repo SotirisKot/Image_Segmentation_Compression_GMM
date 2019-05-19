9b9b36ee1a47b1ba99e9f18110b4e734f51d7a46
sotiris <sotiriskot9@gmail.com>
2019-05-19 21:33:07 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import numpy as np
+from tqdm import tqdm
+from pprint import pprint
+import matplotlib.pyplot as plt
+np.random.seed(0)
+
+
+# a function to calculate the gammas/responsibilities
+def get_gammas():
+    pass
+
+
+# a function to calculate the reconstruction error of the image
+def get_reconstruction_error():
+    pass
+
+
+# a function to calculate the maximum log-likelihood
+def get_log_likelihood():
+    pass
+
+
+# a function to visualize the image
+def visualize_image():
+    pass
+
+
+# a function to train the EM algorithm
+def train():
+    pass
+
+
+# a function that executes a step of the EM algorithm
+def EM_step():
+    pass
+
+
+# a function to initialize all the values that we will need
+def initialize_variables():
+    pass
+
+
+# a function to update the parameters
+def update_parameters():
+    pass
+
+
